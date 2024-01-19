9c7a6bac1124056fecb7ed0ffd16ac4a4737b8b1
Priyanshu Negi <npriyanshu63@gmail.com>
2024-01-19 21:22:57 +0530

    learned : update method , connect and disconnect

--- script.ts
+++ script.ts
@@ -54,16 +54,47 @@ async function main() {
 // },
        
 // ]})
-const user = await prisma.user.findMany({
-    where: {
-        // age:23
-        // name:"pinchu"
-        // name:{equals:"pinchu"}  //- gives users with matching name
-        // name:{not:"pinchu"}, // - gives names which not matched
-        // name:{in:["pinchu",'someothername']}  //- gives users which names are presented in this array
-        // name:{notIn:["pinchu",'someothername']} // - gives users which names are not presented in this array
 
-
+// const user = await prisma.user.findMany({
+//     where: {
+    
+    // queries on relationships
+    //  userPreference:{
+        //      emailUpdates:true
+        //  }
+        
+        // finds every user with writtenpost which have title test
+        // writtenPosts:{
+//     every:{
+    //         title:"Test"
+    //     }
+    // }
+    
+    // returs all users which didn't have this title 
+    //    writtenPosts:{
+        //         none:{
+            //             title:"Test"
+            //         }
+            //     }
+            
+            // returs users which title start with query have this 
+            //    writtenPosts:{
+                //         some:{
+                    //             title:"Test"
+                    //         }
+                    //     }
+                    
+                    
+                    
+                    
+                    // age:23
+                    // name:"pinchu"
+                    // name:{equals:"pinchu"}  //- gives users with matching name
+                    // name:{not:"pinchu"}, // - gives names which not matched
+                    // name:{in:["pinchu",'someothername']}  //- gives users which names are presented in this array
+                    // name:{notIn:["pinchu",'someothername']} // - gives users which names are not presented in this array
+                    
+                    
         // we can also perform and type query by giving two attributes like this 
 
 // name:"pinchu Bhai",
@@ -82,24 +113,63 @@ const user = await prisma.user.findMany({
 
 // email:{startsWith:"pinchu"}
 
-    },
+// },
 //     orderBy:{
-//         age:"asc"
-//     },
-//     // distinct:['name','age']
-//     take:2,
-//     skip:1,
-  });
-  
-
-console.log(user)
-}
+    //         age:"asc"
+    //     },
+    //     // distinct:['name','age']
+    //     take:2,
+    //     skip:1,
+// });
 
 
-main().catch(e => {
-    console.error(e)
-})
-.finally(async () => {
+
+// const user = await prisma.post.findMany({
+    //     where: {
+
+        //         // relationship filter type query
+        // author:{
+            // // is:{
+                // //    age:27 
+                // // }
+                
+                // isNot:{
+                    //     age:27,
+                    // }
+                    // }
+                    //     }
+                    // });
+                    
+                    
+         const user = await prisma.user.update({
+                        where:{
+                            email:"priyanshu1@gmail.com"
+                        },
+                        data:{
+                            email:'priyanshu1@gmail.com',
+                            userPreference:{
+                            //   create:{
+                            //     emailUpdates:true
+                            //   }  
+                            connect:{
+                                id:'4f69a7d7-7ad2-4f0a-8980-744c74b99b9a'
+                            }
+                            // disconnect:{
+                            //     id:'4f69a7d7-7ad2-4f0a-8980-744c74b99b9a'
+                            // }
+                            }
+                        },
+                    })
+
+                    console.log(user)
+                    
+                }
+                
+                
+                main().catch(e => {
+                    console.error(e)
+                })
+                .finally(async () => {
     await prisma.$disconnect()
 
 })
